a6aaf31b60c35e0083a286849cb4b42129b4c3c9
Maid <zhp92@outlook.com>
2022-05-12 00:39:25 +0800

    add Connection

---
+++ znet/connection.go
@@ -0,0 +1,80 @@
+package znet
+
+import (
+	"fmt"
+	"net"
+	"zinx_demo/ziface"
+)
+
+type Connection struct {
+	Conn         *net.TCPConn
+	ConnID       uint32
+	isClosed     bool
+	handleAPI    ziface.HandFunc
+	ExitBuffChan chan bool
+}
+
+func (c *Connection) StartReader() {
+	fmt.Println("Reader Goroutine is  running")
+	defer fmt.Println(c.RemoteAddr().String(), " conn reader exit!")
+	defer c.Stop()
+
+	for {
+		buf := make([]byte, 512)
+		cnt, err := c.Conn.Read(buf)
+		if err != nil {
+			fmt.Println("recv buf err ", err)
+			c.ExitBuffChan <- true
+			return
+		}
+		if err := c.handleAPI(c.Conn, buf, cnt); err != nil {
+			fmt.Println("connID ", c.ConnID, " handle is error")
+			c.ExitBuffChan <- true
+			return
+		}
+	}
+}
+
+func (c *Connection) Start() {
+	go c.StartReader()
+
+	for {
+		select {
+		case <-c.ExitBuffChan:
+			return
+		}
+	}
+}
+
+func (c *Connection) Stop() {
+	if c.isClosed == true {
+		return
+	}
+	c.isClosed = true
+	c.Conn.Close()
+	c.ExitBuffChan <- true
+	close(c.ExitBuffChan)
+}
+
+func (c *Connection) GetTCPConnection() *net.TCPConn {
+	return c.Conn
+}
+
+func (c *Connection) GetConnID() uint32 {
+	return c.ConnID
+}
+
+func (c *Connection) RemoteAddr() net.Addr {
+	return c.Conn.RemoteAddr()
+}
+
+func NewConntion(conn *net.TCPConn, connID uint32, callback_api ziface.HandFunc) *Connection {
+	c := &Connection{
+		Conn:         conn,
+		ConnID:       connID,
+		isClosed:     false,
+		handleAPI:    callback_api,
+		ExitBuffChan: make(chan bool, 1),
+	}
+	return c
+}
--- znet/server.go
+++ znet/server.go
@@ -1,6 +1,7 @@
 package znet
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"time"
@@ -14,43 +15,40 @@ type Server struct {
 	Port      int
 }
 
+func CallBackToClient(conn *net.TCPConn, data []byte, cnt int) error {
+	fmt.Println("[Conn Handle] CallBackToClient ... ")
+	if _, err := conn.Write(data[:cnt]); err != nil {
+		fmt.Println("write back buf err ", err)
+		return errors.New("CallBackToClient error")
+	}
+	return nil
+}
+
 func (s *Server) Start() {
 	fmt.Printf("[START] Server listenner at IP: %s, Port %d, is starting\\n\n", s.IP, s.Port)
-	addr, err := net.ResolveTCPAddr(s.IPVersion, fmt.Sprintf("%s:%d", s.IP, s.Port))
-	if err != nil {
-		fmt.Println("resolve tcp addr err: ", err)
-		return
-	}
-	listenner, err := net.ListenTCP(s.IPVersion, addr)
-	if err != nil {
-		fmt.Println("listen", s.IPVersion, "err", err)
-		return
-	}
-	fmt.Println("start Zinx server  ", s.Name, " succ, now listenning...")
-
 	go func() {
+		addr, err := net.ResolveTCPAddr(s.IPVersion, fmt.Sprintf("%s:%d", s.IP, s.Port))
+		if err != nil {
+			fmt.Println("resolve tcp addr err: ", err)
+			return
+		}
+		listenner, err := net.ListenTCP(s.IPVersion, addr)
+		if err != nil {
+			fmt.Println("listen", s.IPVersion, "err", err)
+			return
+		}
+		fmt.Println("start Zinx server  ", s.Name, " succ, now listenning...")
+		var cid uint32
+		cid = 0
 		for {
 			conn, err := listenner.AcceptTCP()
 			if err != nil {
 				fmt.Println("Accept err ", err)
 				continue
 			}
-
-			go func() {
-				for {
-					buf := make([]byte, 512)
-					cnt, err2 := conn.Read(buf)
-					if err2 != nil {
-						fmt.Println("recv buf err ", err)
-						continue
-					}
-
-					if _, err := conn.Write(buf[:cnt]); err != nil {
-						fmt.Println("write back buf err ", err)
-						continue
-					}
-				}
-			}()
+			dealConn := NewConntion(conn, cid, CallBackToClient)
+			cid++
+			go dealConn.Start()
 		}
 	}()
 }
@@ -61,8 +59,6 @@ func (s *Server) Stop() {
 
 func (s *Server) Serve() {
 	s.Start()
-
-	//阻塞,否则主Go退出， listenner的go将会退出
 	for {
 		time.Sleep(10 * time.Second)
 	}
